7301691b7ef66fdbe9cbecc31b79e0bc8575b30d
David Konsumer <konsumer@jetboystudio.com`>
2022-12-05 02:31:51 -0800

    improved CLI parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,62 @@
 #include <iostream>
 #include <cstdlib>
 #include "BeatStep.hpp"
-#include "cxxopts.hpp"
 
-int main(int argc, char *argv[]) {
-  cxxopts::Options options("beatstep", "Control your BeatStep with sysex");
+#include "CLI/App.hpp"
+#include "CLI/Formatter.hpp"
+#include "CLI/Config.hpp"
 
-  options
-    .positional_help("COMMAND")
-    .show_positional_help();
-
-  options.add_options()
-    // ("b,bar", "Param bar", cxxopts::value<std::string>())
-    // ("d,debug", "Enable debugging", cxxopts::value<bool>()->default_value("false"))
-    ("d,device", "The MIDI device to use", cxxopts::value<int>()->default_value("1"))
-    ("h,help", "Print usage")
-  ;
-
-  options.add_options("list")
-    ("", "List available MIDI-out devices", cxxopts::value<std::vector<std::string>>())
-  ;
-
-  options.parse_positional({"list"});
-
-  auto result = options.parse(argc, argv);
-
-  if (result.count("help")){
-    std::cout << options.help() << std::endl;
-    return EXIT_SUCCESS;
-  }
-
+void listDevices(){
   BeatStep* bs = new BeatStep();
   bs->list();
   delete bs;
-  return EXIT_SUCCESS;
+}
+
+int loadBeatstep(int device, std::string filename) {
+  return 0;
+}
+
+int saveBeatstep(int device, std::string filename) {
+  return 0;
+}
+
+int updateFirmware(int device, std::string filename) {
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  CLI::App app{"Use sysex to control BeatStep"};
+  app.require_subcommand();
+
+  int device = 1;
+  std::string filename;
+
+  auto subList = app.add_subcommand("list", "List available MIDI devices");
+  subList->final_callback(listDevices);
+  
+  auto subLoad = app.add_subcommand("load", "Load a .beatstep preset file into device");
+  subLoad->add_option("-d,--device", device, "The device to use (see list)");
+  subLoad->add_option("file", filename, "The .beatstep file")->required();
+
+  auto subSave = app.add_subcommand("save", "Save a .beatstep preset file from device");
+  subSave->add_option("-d,--device", device, "The device to use (see list)");
+  subSave->add_option("file", filename, "The .beatstep file")->required();
+
+  auto subUpdate = app.add_subcommand("update", "Install a .led firmware file on device");
+  subUpdate->add_option("-d,--device", device, "The device to use (see list)");
+  subUpdate->add_option("file", filename, "The .led file")->required();
+
+  CLI11_PARSE(app, argc, argv);
+
+  if (app.got_subcommand(subLoad)) {
+    return loadBeatstep(device, filename);
+  }
+
+  if (app.got_subcommand(subSave)) {
+    return saveBeatstep(device, filename);
+  }
+
+  if (app.got_subcommand(subUpdate)) {
+    return updateFirmware(device, filename);
+  }
 }
